1515c125e303585b613ea44fd0dc4f3bc6850549
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-11-27 16:24:59 +0100

    RectangleGui class contructor and fields added (Vaadin GUI works)

--- src/main/java/pl/bartekficek/rectangle_app/RectangleGui.java
+++ src/main/java/pl/bartekficek/rectangle_app/RectangleGui.java
@@ -1,5 +1,25 @@
 package pl.bartekficek.rectangle_app;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
 
-public class RectangleGui {
+@Route
+public class RectangleGui extends VerticalLayout {
+
+    private TextField textFieldHeight;
+    private TextField textFieldWidth;
+    private Button button;
+
+    public RectangleGui() {
+        textFieldHeight = new TextField("Write Height");
+        textFieldWidth = new TextField("Write Width");
+        button = new Button("Add");
+
+        add(textFieldHeight);
+        add(textFieldWidth);
+        add(button);
+    }
+
 }
