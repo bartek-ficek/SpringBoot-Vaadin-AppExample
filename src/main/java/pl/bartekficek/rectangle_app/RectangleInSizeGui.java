b20a1f7220fdce9fdb4a7bf26aac937a1cd8c3e4
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-11-30 17:21:38 +0100

    parameter, Field & Text Areas added in  RectangleInSizeGui. APP DONE!

--- src/main/java/pl/bartekficek/rectangle_app/RectangleInSizeGui.java
+++ src/main/java/pl/bartekficek/rectangle_app/RectangleInSizeGui.java
@@ -2,26 +2,36 @@ package pl.bartekficek.rectangle_app;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.awt.*;
 
 @Route
 public class RectangleInSizeGui extends VerticalLayout {
 
     private RectangleRepo rectangleRepo;
-    private TextArea textArea;
+
+    private final com.vaadin.flow.component.textfield.TextField textFieldSize;
+    private com.vaadin.flow.component.button.Button buttonSmallerPerimeter;
+    private com.vaadin.flow.component.button.Button buttonBiggerPerimeter;
+    private TextArea textAreaResult;
 
     @Autowired
     public RectangleInSizeGui(RectangleRepo rectangleRepo) {
         this.rectangleRepo = rectangleRepo;
-        textArea=new TextArea();
-        textArea.setValue(rectangleRepo.getBigRectangles().toString());
+        this.textFieldSize = new TextField("Write rectangle perimeter");
 
-        add(textArea);
+        buttonBiggerPerimeter = new com.vaadin.flow.component.button.Button("Show greater than written");
+        buttonSmallerPerimeter = new com.vaadin.flow.component.button.Button("Show smaller than written");
 
+        buttonBiggerPerimeter.addClickListener(clickEvent -> textAreaResult.setValue(rectangleRepo.getBigRectangles(Integer.parseInt(textFieldSize.getValue())).toString()));
+        buttonSmallerPerimeter.addClickListener(clickEvent -> textAreaResult.setValue(rectangleRepo.getSmallRectangles(Integer.parseInt(textFieldSize.getValue())).toString()));
+
+        textAreaResult = new TextArea("Result");
+
+        add(textFieldSize);
+        add(buttonBiggerPerimeter);
+        add(buttonSmallerPerimeter);
+        add(textAreaResult);
     }
-
-
 }
--- src/main/java/pl/bartekficek/rectangle_app/RectangleRepo.java
+++ src/main/java/pl/bartekficek/rectangle_app/RectangleRepo.java
@@ -2,6 +2,7 @@ package pl.bartekficek.rectangle_app;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +12,9 @@ public interface RectangleRepo extends CrudRepository<Rectangle, Long> {
 
     String PERIMETER = "2 * rect.height + 2 * rect.width";
 
-    @Query(nativeQuery = true, value = "SELECT * FROM RECTANGLE rect WHERE (" + PERIMETER + ">= 20)")
-    List<Rectangle> getBigRectangles();
+    @Query(nativeQuery = true, value = "SELECT * FROM RECTANGLE rect WHERE (" + PERIMETER + ">= :rectanglePerimeter)")
+    List<Rectangle> getBigRectangles(@Param("rectanglePerimeter") int rectanglePerimeter);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM RECTANGLE rect WHERE " + PERIMETER + " < 20")
-    List<Rectangle> getSmallRectangles();
+    @Query(nativeQuery = true, value = "SELECT * FROM RECTANGLE rect WHERE " + PERIMETER + " < :rectanglePerimeter")
+    List<Rectangle> getSmallRectangles(@Param("rectanglePerimeter") int rectanglePerimeter);
 }
