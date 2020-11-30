9a4f2eb6e08b0af0e201bd5eba94f371ff661e5e
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-11-30 10:15:11 +0100

    class FigureAPI with method calculate created

---
+++ src/main/java/pl/bartekficek/rectangle_app/FigureAPI.java
@@ -0,0 +1,19 @@
+package pl.bartekficek.rectangle_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FigureAPI {
+
+    @GetMapping("/calculate")
+    public int calculate(@RequestParam String figure, @RequestParam int sideA, @RequestParam int sideB, @RequestParam int sideC) {
+        if (figure.equalsIgnoreCase("TRIANGLE")) {
+            return sideA + sideB + sideC;
+        } else if (figure.equalsIgnoreCase("RECTANGLE")) {
+            return 2 * sideA + 2 * sideB;
+        }
+        return 0;
+    }
+}
